55f4ad238af55978b4df71a226b18015997e4101
Sebastian Potrykus <seba05092001@gmail.com>
2023-09-08 12:49:04 +0200

    Spring initializer

---
+++ src/main/java/com/sebpot/cyclecompete/CyclecompeteApplication.java
@@ -0,0 +1,13 @@
+package com.sebpot.cyclecompete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclecompeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyclecompeteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebpot/cyclecompete/CyclecompeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebpot.cyclecompete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyclecompeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
